e74c9d8213e2ea1e219a4aaf7f785c93e095e20d
DHONDGE YASHVARDHAN CHATURBHUJ <yashvardhandhondgepc@outlook.com>
2025-01-15 14:12:50 +0530

    Update index.js

--- pdf_analyzer/Backend/index.js
+++ pdf_analyzer/Backend/index.js
@@ -8,7 +8,7 @@ import dotenv from "dotenv"
 dotenv.config()
 const app = express();
 const corsOptions = {
-    origin: "https://assistant-chatbot-ikjm.vercel.app/", 
+    origin: "*", 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
     credentials: true, 
   };
